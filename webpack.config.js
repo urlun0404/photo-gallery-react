c4b8878ebdb8a5380e1b168b0e18cb56f5b4e8ae
Lauren Lin <urlun0404@gmail.com>
2022-11-09 16:03:23 +0800

    Fix .env file reading issue. The issue only can be solved through third-party webpack-dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: {
@@ -28,6 +29,10 @@ module.exports = {
     },
   },
   plugins: [
+    new Dotenv({
+      path: './.env',
+      safe: true,
+    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './public/index.html',
