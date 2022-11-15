045883dbfe3e263b7985f5a7bcaa48fc1432abe3
Lauren Lin <urlun0404@gmail.com>
2022-11-16 03:01:12 +0800

    Add basename to BrowserRouter. For solving react router issue that cannot find the root path after deployment

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </Provider>
