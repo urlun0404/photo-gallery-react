4948d4efacf27fa71e2cd79ddf0c84afb2f6efb2
Lauren Lin <urlun0404@gmail.com>
2022-11-16 01:49:06 +0800

    Remove comments

--- src/hooks/use-fetch.js
+++ src/hooks/use-fetch.js
@@ -1,176 +1,19 @@
-import * as Img from 'images';
 import { useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { photoActions } from 'store/photo-slice';
-import { useNavigate } from 'react-router-dom';
-
-const test_photos = [
-  {
-    id: 1,
-    user: 'Céline',
-    webformatURL: Img.jessika,
-    largeImageURL: Img.jessika,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 2,
-    user: 'Céline',
-    webformatURL: Img.harper,
-    largeImageURL: Img.harper,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 3,
-    user: 'Céline',
-    webformatURL: Img.céline,
-    largeImageURL: Img.céline,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 4,
-    user: 'Dominika Mazur',
-    webformatURL: Img.dominika,
-    largeImageURL: Img.dominika,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 5,
-    user: 'Céline',
-    webformatURL: Img.jessika,
-    largeImageURL: Img.jessika,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 6,
-    user: 'Céline',
-    webformatURL: Img.harper,
-    largeImageURL: Img.harper,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 7,
-    user: 'Céline',
-    webformatURL: Img.céline,
-    largeImageURL: Img.céline,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 8,
-    user: 'Dominika Mazur',
-    webformatURL: Img.dominika,
-    largeImageURL: Img.dominika,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 9,
-    user: 'Céline',
-    webformatURL: Img.jessika,
-    largeImageURL: Img.jessika,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 10,
-    user: 'Céline',
-    webformatURL: Img.harper,
-    largeImageURL: Img.harper,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 11,
-    user: 'Céline',
-    webformatURL: Img.céline,
-    largeImageURL: Img.céline,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 12,
-    user: 'Dominika Mazur',
-    webformatURL: Img.dominika,
-    largeImageURL: Img.dominika,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 13,
-    user: 'Céline',
-    webformatURL: Img.harper,
-    largeImageURL: Img.harper,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 14,
-    user: 'Céline',
-    webformatURL: Img.céline,
-    largeImageURL: Img.céline,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-  {
-    id: 15,
-    user: 'Dominika Mazur',
-    webformatURL: Img.dominika,
-    largeImageURL: Img.dominika,
-    views: 25,
-    likes: 100,
-    downloads: 100,
-    tags: 'a, b, c',
-  },
-];
 
 export default function useFetch() {
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
   const [errorMessages, setErrorMessages] = useState(false);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const fetchData = useCallback(async (url, overLoad, isNewSearch = false) => {
     setHasError(false);
     setIsLoading(true);
     try {
-      // const response = await fetch(url, overLoad);
+      const response = await fetch(url, overLoad);
       const data = await response.json();
-      // console.log(data);
       if (!response.ok || Number(data['total']) === 0) {
         throw new Error(
           response.statusText || "Couldn't find any matched photos",
@@ -183,16 +26,9 @@ export default function useFetch() {
           }),
         );
       }
-      // dispatch(
-      //   photoActions.setPhotos({
-      //     isNewSearch,
-      //     newPhotos: test_photos,
-      //   }),
-      // );
     } catch (err) {
       setHasError(true);
       setErrorMessages(err);
-      // navigate('/error');
       console.error(err);
     }
     setIsLoading(false);
